f0d113e78a4d350d6c30c4345775d024542f17f1
remgo696 <37494165+remgo696@users.noreply.github.com>
2026-02-25 01:20:48 -0500

    Add project configuration file for Clock24H12H in NetBeans format

---
+++ Clock24H12H.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Clock24H12H.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Clock24H12H.X.production.hex
